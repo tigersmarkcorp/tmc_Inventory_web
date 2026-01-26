3336b1ffa80e76d5e73cae4ae26fb0a93cc93c9d
tigersmarkcorp <156992403+tigersmarkcorp@users.noreply.github.com>
2026-01-26 18:42:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -33,12 +33,12 @@ export default defineConfig(({ mode }) => ({
             type: "image/png",
           },
           {
-            src: "/inventoryBG.png",
+            src: "/inventoryT.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/inventoryBG.png",
+            src: "/inventoryT.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
